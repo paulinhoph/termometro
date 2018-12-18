da9c715590741e21109776a25c30338798cb5f21
paulinhoph <paulinhoph.ph67@gmail.com>
2018-12-18 13:05:09 -0200

    ajeitando css

--- src/components/TelaPrimaria/Chave.js
+++ src/components/TelaPrimaria/Chave.js
@@ -8,22 +8,38 @@ export default class Chave extends Component {
     constructor(props) {
       super(props);
       this.state = {value: ''};
-    }
-  
-  
+
+      this.handleChange = this.handleChange.bind(this);
+      this.handleClick = this.handleClick.bind(this);
+      } 
+
+      handleChange(event){
+        this.setState({ value: event.target.value });
+        console.log(this.state.value)
+      }
+      handleClick() {
+      }    
   
     render() {
-
+      if ( this.handleClick == "alohomora") {
+        console.log("Usuário autenticado");   
+    } 
     return (
       <div className="App-Chave">
             <img className="logo-temometro" src={logo} /> 
             <h2 className="text-Chave">Digite a palavra mágica:</h2>
                           
             <input 
+              value={this.state.value}
+                onChange={this.handleChange}
                   type="chave"  
                     className="text-Chave-Input"
-                       placeholder="mágica" />
-            <Link className="App-textButton" to="/Temperaturas"  >Ok</Link>
+                       placeholder="Digite Aqui" />
+            <Link 
+              onClick={this.handleClick}
+                className="App-textButton"
+                  to="/Temperaturas">
+                  Ok</Link>
       </div> 
     );
   }
